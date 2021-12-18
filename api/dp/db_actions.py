1b672fe4367cc932c2cfb704dea04a19e732118a
Atahan Aksoy <aaksoy17@ku.edu.tr>
2021-12-18 17:06:12 +0300

    Add DP implementations

---
+++ api/dp/db_actions.py
@@ -0,0 +1,40 @@
+import sqlite3
+
+DB_PATH = "../db/medical_db.db"
+TABLE_NAME = "data"
+
+con = sqlite3.connect(DB_PATH)
+
+
+def get_column_names():
+    cursor = con.execute(f"SELECT * FROM {TABLE_NAME}")
+    names = list(map(lambda x: x[0], cursor.description))
+    return names[1:]
+
+
+def get_count_of_attr(attr_name):
+    cursor = con.execute(f"SELECT {attr_name}, COUNT({attr_name}) FROM {TABLE_NAME} GROUP BY {attr_name}")
+    res = dict()
+    for k,v in cursor.fetchall():
+        res[k] = v
+
+    return res
+
+
+def get_attr_values_from_name(attr_name):
+    cursor = con.execute(f"SELECT DISTINCT {attr_name} FROM {TABLE_NAME}")
+    res = []
+    for tup in cursor.fetchall():
+        res.append(tup[0])
+
+    return res
+
+
+def get_count_of_attr_w_condition(attr1_name, attr1_val, attr2_name):
+    cursor = con.execute(f"SELECT {attr2_name}, COUNT({attr2_name}) FROM {TABLE_NAME} WHERE {attr1_name} = '{attr1_val}' GROUP BY {attr2_name}")
+    res = dict()
+
+    for k,v in cursor.fetchall():
+        res[k] = v
+
+    return res
--- api/dp/dp.py
+++ api/dp/dp.py
@@ -1,10 +1,19 @@
-import sqlite3
+from db_actions import *
+import numpy as np
 
-DB_PATH = "../db/medical_db.db"
+BUDGET = 2
 
-con = sqlite3.connect(DB_PATH)
 
-cur = con.cursor()
+def add_laplace_noise(real_answer: list, sensitivity: float, epsilon: float):
+    scale = sensitivity / epsilon
+    return [answer + np.random.laplace(scale=scale) for answer in real_answer]
 
-for row in cur.execute('SELECT * FROM data'):
-    print(row)
+
+def get_noisy_count_of_attr(attr_name):
+    count_dict = get_count_of_attr(attr_name)
+    noisy_result = add_laplace_noise(count_dict.values(), 1, BUDGET)
+
+    for i, k in enumerate(count_dict.keys()):
+        count_dict[k] = noisy_result[i]
+
+    return count_dict
